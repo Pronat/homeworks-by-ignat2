7326837f76a3c3310b9d3466d35e51f84403a4c7
AlexPron <Fotolinza@gmail.com>
2022-10-24 21:47:36 +0300

    lesson1-3

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from '../logo.svg';
+import logo from '../../logo.svg';
 import './App.css';
 
 function App() {
@@ -7,8 +7,7 @@ function App() {
     <div className="App">
       {/*hashrouter, provider*/}
       <>
-        {/*<Header />*/}
-        {/*<Main />*/}
+        {/*/!*<Main />*!/  header и router отрисуем в Main */}
 
       </>
 
