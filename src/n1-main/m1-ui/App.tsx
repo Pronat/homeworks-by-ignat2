458d901fa03fe23acb2eaaa646597dbfb74e47a4
AlexPron <Fotolinza@gmail.com>
2022-10-29 23:19:30 +0300

    lesson1-8

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from '../../logo.svg';
 import './App.css';
+import TestComponents from "./pages/test-components";
 
 function App() {
   return (
     <div className="App">
+        <TestComponents/>
       {/*hashrouter, provider*/}
       <>
         {/*/!*<Main />*!/  header и router отрисуем в Main */}
--- src/n1-main/m1-ui/pages/test-components.tsx
+++ src/n1-main/m1-ui/pages/test-components.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import SuperButton from "../common/c2-SuperButton/SuperButton";
+import SuperInputText from "../common/c1-SuperInputText/SuperInputText";
+import SuperCheckbox from "../common/c3-SuperCheckbox/SuperCheckbox";
 
 const TestComponents = () => {
     return (
         <div>
             TestComponents
+            <div><SuperButton>test button</SuperButton></div>
+            <div><SuperInputText></SuperInputText></div>
+            <div><SuperCheckbox></SuperCheckbox></div>
         </div>
     );
 };
