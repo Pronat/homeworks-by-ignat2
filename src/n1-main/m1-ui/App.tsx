d45d71f4af55abdc68331739d18e11c00b9a81c1
AlexPron <Fotolinza@gmail.com>
2022-10-30 17:19:52 +0300

    lesson1-13

--- src/n1-main/m1-ui/App.tsx
+++ src/n1-main/m1-ui/App.tsx
@@ -3,23 +3,16 @@ import './App.css';
 import {HashRouter, Route, Router, Routes} from "react-router-dom";
 import Profile from "./pages/profile";
 import Error404 from "./pages/Error404";
+import {Header} from "./header/Header";
+import {Pages} from "./pages/Pages";
 
 function App() {
   return (
     <div className="App">
         <HashRouter>
-
+            <Header />
+            <Pages />
         </HashRouter>
-
-            {/*<div>*/}
-            {/*    <Routes>*/}
-            {/*        <Route path="/" element={<App />} />*/}
-            {/*        <Route path="/pages/profile" element={<Profile />} />*/}
-            {/*        <Route path="/pages/Error404" element={<Error404 />} />*/}
-            {/*    </Routes>*/}
-            {/*</div>*/}
-
-
 
 
 
--- src/n1-main/m1-ui/header/Header.tsx
+++ src/n1-main/m1-ui/header/Header.tsx
@@ -6,12 +6,12 @@ import {PATH} from "../pages/Pages";
 export function Header() {
     return (
         <div className={s.header}>
-            <NavLink to={PATH.ENTER_NEW_PASSWORD} className={s.link}>pre-junior</NavLink>
-            <NavLink to={PATH.LOGIN} className={s.link}>junior</NavLink>
-            <NavLink to={PATH.PROFILE} className={s.link}>junior+</NavLink>
-            <NavLink to={PATH.REGISTRATION} className={s.link}>junior+</NavLink>
-            <NavLink to={PATH.RESTORE_PASSWORD} className={s.link}>junior+</NavLink>
-            <NavLink to={PATH.TEST_COMPONENTS} className={s.link}>junior+</NavLink>
+            <NavLink to={PATH.ENTER_NEW_PASSWORD} className={s.link}>Enter new password</NavLink>
+            <NavLink to={PATH.LOGIN} className={s.link}>Login</NavLink>
+            <NavLink to={PATH.PROFILE} className={s.link}>Profile</NavLink>
+            <NavLink to={PATH.REGISTRATION} className={s.link}>Registration</NavLink>
+            <NavLink to={PATH.RESTORE_PASSWORD} className={s.link}>Restore password</NavLink>
+            <NavLink to={PATH.TEST_COMPONENTS} className={s.link}>Test Components</NavLink>
         </div>
     )
 }
