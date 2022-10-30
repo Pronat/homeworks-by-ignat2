b72bd5797059fc7d7d3f85a5501cb5afdcef9b1c
AlexPron <Fotolinza@gmail.com>
2022-10-30 20:13:51 +0300

    lesson1-16

---
+++ src/n1-main/m2-bll/store.tsx
@@ -0,0 +1,14 @@
+import {applyMiddleware, combineReducers, createStore} from "redux";
+import thunk from "redux-thunk";
+
+const rootReducer = combineReducers({
+    login: loginReducer,
+    register: registerReducer
+})
+
+export const store = createStore(rootReducer, applyMiddleware(thunk))
+
+export type AppRootStateType = ReturnType<typeof rootReducer>
+
+// @ts-ignore
+window.store = store
