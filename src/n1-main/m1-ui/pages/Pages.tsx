d550f603fab8f78b110097edbde61d37fb98a744
AlexPron <Fotolinza@gmail.com>
2022-10-30 17:12:02 +0300

    lesson1-11

--- src/n1-main/m1-ui/pages/Pages.tsx
+++ src/n1-main/m1-ui/pages/Pages.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import {Navigate, Route, Routes} from "react-router-dom";
 import Error404 from "./Error404";
 import EnterNewPassword from "./enter-new-password";
+import {Login} from "./login";
+import Profile from "./profile";
+import Registration from "./registration";
+import RestorePassword from "./restore-password";
+import TestComponents from "./test-components";
 
 export const PATH = {
     ENTER_NEW_PASSWORD: "/enter-new-password",
+    LOGIN: "/login",
+    PROFILE: "/profile",
+    REGISTRATION: "/registration",
+    RESTORE_PASSWORD: "/restore-password",
+    TEST_COMPONENTS: "/test-components"
+
 }
 
 export const Pages = () => {
@@ -14,15 +25,13 @@ export const Pages = () => {
             <Routes>
 
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
-
-                <Route path={PATH.PRE_JUNIOR} element={<EnterNewPassword/>}/>
-                <Route path={PATH.JUNIOR} element={<Junior/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
-                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
+                <Route path={PATH.ENTER_NEW_PASSWORD} element={<EnterNewPassword/>}/>
+                <Route path={PATH.LOGIN} element={<Login/>}/>
+                <Route path={PATH.PROFILE} element={<Profile/>}/>
+                <Route path={PATH.REGISTRATION} element={<Registration/>}/>
+                <Route path={PATH.RESTORE_PASSWORD} element={<RestorePassword/>}/>
+                <Route path={PATH.TEST_COMPONENTS} element={<TestComponents/>}/>
+                <Route path={'/*'} element={<Error404/>}/>
 
 
                 {/*// add routes*/}
