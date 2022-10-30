5d0b2c1635c6b97f5959dba83b4558571bdab0aa
AlexPron <Fotolinza@gmail.com>
2022-10-30 20:16:50 +0300

    lesson1-17

--- src/n1-main/m2-bll/store.tsx
+++ src/n1-main/m2-bll/store.tsx
@@ -1,9 +1,12 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunk from "redux-thunk";
+import profile from "../m1-ui/pages/profile";
 
 const rootReducer = combineReducers({
     login: loginReducer,
-    register: registerReducer
+    register: registerReducer,
+    profile: profileReducer,
+
 })
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
